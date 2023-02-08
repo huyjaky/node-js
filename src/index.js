0b1fd18bc1ca56607eef52e2751d28f2cea0b3f6
huyjaky <huyphanuyg@gmail.com>
2023-02-08 19:58:17 +0700

    setup thu vien

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const morgan = require('morgan');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express()
+const port = 3000
+
+// lay file anh tu public/img 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// dinh dang du lieu khi keo tu mongo xuong
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// setup handlebars
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// 
+
