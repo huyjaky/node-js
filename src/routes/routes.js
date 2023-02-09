06a4e8318ca2875065ee47971306d2b311e837c6
huyjaky <huyphanuyg@gmail.com>
2023-02-09 13:53:33 +0700

    slider

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,8 +4,8 @@ function route(app) {
     app.use('/homepage', homepage);
 
     app.get('/check', (req, res, next) => {
-        res.render('slider')
-    })
+        res.render('slider');
+    });
 }
 
 module.exports = route;
