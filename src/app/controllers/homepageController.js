ab2521c3a82287292c6c7d322b7c8ee744987d5b
huyjaky <huyphanuyg@gmail.com>
2023-03-02 21:14:37 +0700

    them muc man hinh

--- src/app/controllers/homepageController.js
+++ src/app/controllers/homepageController.js
@@ -5,16 +5,18 @@ const fil = require('./filter/fil');
 class homepageController {
     async homepage(req, res, next) {
         // res.send('check')
-        const [products_hp, products_kb, user_dt] = await Promise.all([
+        const [products_hp, products_kb, products_mn, user_dt] = await Promise.all([
             product.find({ descript: 'headphone' }),
             product.find({
                 $or: [{ descript: 'keyboard' }, { descript: 'keycap' }],
             }),
+            product.find({ descript: 'monitor' }),
             user.findOne({userName: 'Admin'})
         ]);
 
         const products_hp_temp = fil.filter(products_hp);
         const products_kb_temp = fil.filter(products_kb);
+        const products_mn_temp = fil.filter(products_mn);
         const user_temp = user_dt.toObject();
         const cart = user_dt.cart;
 
@@ -22,6 +24,7 @@ class homepageController {
         const data = {
             products_hp: products_hp_temp,
             products_kb: products_kb_temp,
+            products_mn: products_mn_temp,
             user: user_temp,
             cart: cart.length
         };
